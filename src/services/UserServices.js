56ca63424b947960c1d345a495692723ad9594a4
wsiddiqui-nisum-com <waqas.siddiqui@science37.com>
2021-10-08 02:22:05 +0500

    added security in api

--- src/services/UserServices.js
+++ src/services/UserServices.js
@@ -3,7 +3,14 @@ import { BASE_URL } from "./config";
 const UserService = {
   async getUsers() {
     try {
-      return fetch(BASE_URL + "1.1/?results=15").then((response) =>
+      return fetch(BASE_URL + "1.1/?results=15",{
+        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        mode: 'cors', // no-cors, *cors, same-origin
+        cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        referrerPolicy: 'no-referrer').then((response) =>
         response.json()
       );
     } catch (error) {
