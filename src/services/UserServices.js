d3950cd55d454d9f748f17abde9ca257f8e004b9
wsiddiqui-nisum-com <waqas.siddiqui@science37.com>
2021-10-08 02:47:05 +0500

    Updated Security in API

--- src/services/UserServices.js
+++ src/services/UserServices.js
@@ -4,14 +4,20 @@ const UserService = {
   async getUsers() {
     try {
       return fetch(BASE_URL + "1.1/?results=15",{
-        method: 'POST', // *GET, POST, PUT, DELETE, etc.
+        method: 'GET', // *GET, POST, PUT, DELETE, etc.
         mode: 'cors', // no-cors, *cors, same-origin
         cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
+        credentials: 'same-origin', // include, *same-origin, omit
         headers: {
           'Content-Type': 'application/json'
         },
-        referrerPolicy: 'no-referrer').then((response) =>
-        response.json()
+        referrerPolicy: 'no-referrer'}).then((response) => {
+          const contentType = response.headers.get('content-type');
+          if (!contentType || !contentType.includes('application/json')) {
+            throw new TypeError("Oops, we haven't got JSON!");
+          }
+          return response.json();
+        }
       );
     } catch (error) {
       throw error;
